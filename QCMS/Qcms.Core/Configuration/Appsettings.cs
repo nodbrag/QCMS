d09feb99f6630fd40496cb4aa8e2ef65ccc5eb4e
jime.zhu <nodbrag@sina.com>
2019-04-23 15:27:02 +0800

    add Configuration/Appsettings.cs

---
+++ QCMS/Qcms.Core/Configuration/Appsettings.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Qcms.Core.Configuration
+{
+    public class Appsettings
+    {
+
+        /// <summary>
+        /// 静态构造函数
+        /// </summary>
+        static Appsettings()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        /// <summary>
+        /// 配置
+        /// </summary>
+        private static IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// Shorthand for GetSection("ConnectionStrings")[name].
+        /// </summary>
+        /// <param name="name">The connection string key.</param>
+        /// <returns>The value</returns>
+        public static string GetConnectionString(string name)
+        {
+            return Configuration.GetConnectionString(name);
+        }
+
+        /// <summary>
+        /// Gets a configuration sub-section with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the configuration section.</param>
+        /// <returns>The <see cref="T:Microsoft.Extensions.Configuration.IConfigurationSection" />.</returns>
+        /// <remarks>
+        ///     This method will never return <c>null</c>. If no matching sub-section is found with the specified key,
+        ///     an empty <see cref="T:Microsoft.Extensions.Configuration.IConfigurationSection" /> will be returned.
+        /// </remarks>
+        public static IConfigurationSection GetSection(string key)
+        {
+            return Configuration.GetSection(key);
+        }
+
+        /// <summary>
+        /// Attempts to bind the configuration instance to a new instance of type T.
+        /// If this configuration section has a value, that will be used.
+        /// Otherwise binding by matching property names against configuration keys recursively.
+        /// </summary>
+        /// <typeparam name="T">The type of the new instance to bind.</typeparam>
+        /// <param name="configuration">The configuration instance to bind.</param>
+        /// <returns>The new instance of T if successful, default(T) otherwise.</returns>
+        public static T Get<T>()
+        {
+            return Configuration.Get<T>();
+        }
+
+        /// <summary>
+        /// Extracts the value with the specified key and converts it to type T.
+        /// </summary>
+        /// <typeparam name="T">The type to convert the value to.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="key">The key of the configuration section's value to convert.</param>
+        /// <returns>The converted value.</returns>
+        public static T GetValue<T>(string key)
+        {
+            return Configuration.GetValue<T>(key);
+        }
+
+        /// <summary>
+        /// Extracts the value with the specified key and converts it to type T.
+        /// </summary>
+        /// <typeparam name="T">The type to convert the value to.</typeparam>
+        /// <param name="key">The key of the configuration section's value to convert.</param>
+        /// <param name="defaultValue">The default value to use if no value is found.</param>
+        /// <returns>The converted value.</returns>
+        public static T GetValue<T>(string key, T defaultValue)
+        {
+            return (T)Configuration.GetValue(typeof(T), key, defaultValue);
+        }
+    }
+}
